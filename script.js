2c88c1353e06c070a0cbe1c3e6e281d0c56b3def
Valkryn3 <159496206+Valkryn3@users.noreply.github.com>
2024-03-08 18:32:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let slideIndex = 0;
-showSlides();
+ var slideIndex = 0;
+  showSlides();
 
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
